dd3a010bfbecef3f957d623fd587e53e02d4a673
PrographerMan <morozov.salevii@gmail.com>
2022-07-31 13:07:35 +0500

    Examples updated

--- imGUITestStand.js
+++ imGUITestStand.js
@@ -2,93 +2,122 @@
 
 const FRAME_ID = 'ImGUITestStand';
 
-let barsCheckboxValue = false;
-
-function toggleCheckboxValue() {
-    barsCheckboxValue = !barsCheckboxValue;
-}
+let menuVisible = false;
+let comboValue = 0;
+let radioValue = 0;
+let sliderFloatValue = 0;
+let sliderIntValue = 0;
+let selectableValue = false;
+let menuItemValue = false;
+let mainMenuCheckboxValue = false;
+let simpleButtonValue = false;
 
 // Draws a notification in top left window's corner
 ImGui.SetMessage('Notification 1');
 ImGui.SetMessage('Notification 2');
 ImGui.SetMessage('Notification 3');
 
+function toggleVisibility() {
+    menuVisible = !menuVisible;
+}
+
 while (true) {
     wait(0);
 
-    ImGui.BeginFrame(FRAME_ID);
-    
-        // Main menu bar (drawing start)
-        ImGui.BeginMainMenuBar(FRAME_ID);
-            // Menu items
-            ImGui.MenuItem('Menu item 1', true, true);
-            ImGui.MenuItem('Menu item 2', true, true);
-            ImGui.MenuItem('Menu item 3', true, false);
-            
-            ImGui.Button('Bars button', 160.0, 36.0);
-            ImGui.Checkbox('Bars checkbox', barsCheckboxValue);
-            
-            // change checkbox's value if it was clicked
-            if (ImGui.IsItemClicked(FRAME_ID)) {
-                toggleCheckboxValue();
-            }
+    if (Pad.IsKeyUp(20)) {
+        toggleVisibility();
+    }
 
-            // add some spacing for next widget
-            ImGui.Spacing();
-
-            // bars text
-            ImGui.Text('Bars text');
-        // Main menu bar (drawing end)
-        ImGui.EndMainMenuBar();
-
+    if (menuVisible) {
+        ImGui.BeginFrame(FRAME_ID);
         
-        // Window (start drawing)
-        ImGui.Begin("ImGUI Test Stand", true, false, false, false, false);
-            ImGui.SetCursorVisible(true);
+            // Main menu bar (drawing start)
+            ImGui.BeginMainMenuBar(FRAME_ID);
+                // Menu items
+                menuItemValue = ImGui.MenuItem('Menu item 1', menuItemValue, true);
+                ImGui.MenuItem('Menu item 2', true, true);
+                ImGui.MenuItem('Menu item 3', true, false);
+                
+                ImGui.Button('Bars button', 160.0, 36.0);
+                mainMenuCheckboxValue = ImGui.Checkbox('Bars checkbox', mainMenuCheckboxValue);
 
-            // Simple button
-            ImGui.Button('Simple button 1', 160.0, 50.0);
+                // add some spacing for next widget
+                ImGui.Spacing();
+
+                // bars text
+                ImGui.Text('Bars text');
+            // Main menu bar (drawing end)
+            ImGui.EndMainMenuBar();
+
             
-            // This function draws widget in same line than previous
-            ImGui.SameLine();
+            // Window (start drawing)
+            ImGui.Begin("ImGUI Test Stand", true, false, false, false, false);
+                ImGui.SetCursorVisible(true);
 
-            // Another button
-            ImGui.Button('Simple button 2', 160.0, 50.0);
-            ImGui.SameLine();
-            ImGui.Button('Simple button 3', 160.0, 50.0);
+                // Simple button
+                ImGui.Button('Simple button 1', 160.0, 50.0);
+                
+                // This function draws widget in same line than previous
+                ImGui.SameLine();
 
-            // Colored button
-            ImGui.ButtonColored('Button Colored', 255, 0, 0, 1.0, 50.0, 50.0);
+                // Another button
+                ImGui.Button('Simple button 2', 160.0, 50.0);
+                ImGui.SameLine();
 
+                simpleButtonValue = ImGui.Button('Simple button 3', 160.0, 50.0);
+                ImGui.Text(`Simple button value is ${simpleButtonValue}`);
+                
+                if (simpleButtonValue) {
+                    ImGui.SetMessage('Simple button was clicked');
+                }
 
-            ImGui.Text('Simple text');
-            ImGui.TextDisabled('Disabled text');
-            ImGui.TextWithBullet('Text with bullet');
-            ImGui.TextWrapped('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
-            ImGui.TextColored('Colored text', 0, 255.0, 0.0, 1.0);
+                // Colored button
+                ImGui.ButtonColored('Button Colored', 255, 0, 0, 1.0, 50.0, 50.0);
 
-            ImGui.NewLine();
-            ImGui.Separator();
-            ImGui.NewLine();
+                ImGui.Text('Simple text');
+                ImGui.TextDisabled('Disabled text');
+                ImGui.TextWithBullet('Text with bullet');
+                ImGui.TextWrapped('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+                ImGui.TextColored('Colored text', 0, 255.0, 0.0, 1.0);
 
-            // Sliders
-            ImGui.SliderInt('Slider with integer value', 0, 0, 10);
-            ImGui.SliderFloat('Slider with float value', 0, 0, 20.0);
+                ImGui.NewLine();
+                ImGui.Separator();
+                ImGui.NewLine();
 
-            // Text with tooltip
-            ImGui.Text('Hover me to show tooltip');
-            
-            if (ImGui.IsItemHovered(FRAME_ID)) {
-                ImGui.SetTooltip('This is tooltip');
-            }
+                // Sliders
+                sliderIntValue = ImGui.SliderInt('Slider with integer value', 0, 0, 10);
+                sliderFloatValue = ImGui.SliderFloat('Slider with float value', 0, 0, 20.0);
 
-            ImGui.CollapsingHeader('Collapsed header');
+                ImGui.Text(`Slider float value is: ${sliderFloatValue}`);
+                ImGui.SameLine();
+                ImGui.Text(`Slider int value is: ${sliderIntValue}`);
 
-            ImGui.ComboBox('Combo', 'Item 1,Item 2,Item 3', 1);
+                // Text with tooltip
+                ImGui.Text('Hover me to show tooltip');
+                
+                if (ImGui.IsItemHovered(FRAME_ID)) {
+                    ImGui.SetTooltip('This is tooltip');
+                }
 
-            // Selectable widget
-            ImGui.Selectable('Selectable');
-        ImGui.End();
+                // Collapsing header
+                if (ImGui.CollapsingHeader('Collapsing header')) {
+                    ImGui.Text('Collapsing header text');
+                }
 
-    ImGui.EndFrame();
+                comboValue = ImGui.ComboBox('Combo', 'Item 1,Item 2,Item 3', comboValue);
+
+                radioValue = ImGui.RadioButton('Radio Button 1', radioValue, 0);
+                radioValue = ImGui.RadioButton('Radio Button 2', radioValue, 1);
+                radioValue = ImGui.RadioButton('Radio Button 3', radioValue, 2);
+
+                ImGui.Text(`Radio button value is: ${radioValue}`);
+
+                // Selectable widget
+                if (ImGui.Selectable('Selectable 1')) {
+                    ImGui.Text('Selectable');
+                }
+            ImGui.End();
+
+        ImGui.EndFrame();
+    }
 }
